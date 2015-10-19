f16a0412c16c9079d88637eafa0d9d53c06e63e3
Usu√°rio Convidado <Guest@l2304micro30.local>
2015-10-19 19:25:01 -0200

    Initial Commit

---
+++ GMGuiaTurismo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GMGuiaTurismo
+//
+//  Created by Usuário Convidado on 19/10/15.
+//  Copyright © 2015 Gustavo Morrell RM 49449. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GMGuiaTurismoTests/GMGuiaTurismoTests.swift
@@ -0,0 +1,36 @@
+//
+//  GMGuiaTurismoTests.swift
+//  GMGuiaTurismoTests
+//
+//  Created by Usuário Convidado on 19/10/15.
+//  Copyright © 2015 Gustavo Morrell RM 49449. All rights reserved.
+//
+
+import XCTest
+@testable import GMGuiaTurismo
+
+class GMGuiaTurismoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
